1809ff9ff3947390956be703a9fbdbd706b1a2d7
danielaibar1994 <61664677+danielaibar1994@users.noreply.github.com>
2023-12-02 18:25:37 +0100

    feat: Add Toaster & Styles refactor (#56)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LoaderComponent } from './shared/components/loader/loader.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, LoaderComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, LoaderComponent, NavbarComponent],
 })
 export class AppComponent {}
--- src/app/pages/customer-list/customer-list.component.ts
+++ src/app/pages/customer-list/customer-list.component.ts
@@ -6,16 +6,20 @@ import { EventsStorageService } from 'src/app/shared/services/events-storage.ser
 import { Observable } from 'rxjs';
 import { ExternalProduct } from 'src/app/shared/interfaces/products.interface';
 import { SUPERMARKETS } from './constants/supermarkets';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping, faListCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [CommonModule, SupermarketViewComponent],
+  imports: [CommonModule, SupermarketViewComponent, FontAwesomeModule],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css',
 })
 export class CustomerListComponent {
   supermarkets = SUPERMARKETS;
+  faCartShopping = faCartShopping;
+  faListCheck = faListCheck;
 
   products$ = this.store.myDataObservable$ as Observable<ExternalProduct[]>;
 
